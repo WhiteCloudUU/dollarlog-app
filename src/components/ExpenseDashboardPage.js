1baee451f22836eaf12d3b8311c06b4c3c3f0691
Jinzhao Chang <jinzhao@Jinzhaos-MacBook-Pro.local>
2021-05-11 09:15:16 -0700

    Setting up my dollarlog app

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExpenseDashboardPage = () => (
+    <div>
+        Expense dashboard
+    </div>
+);
+
+export default ExpenseDashboardPage;
---
+++ src/playground/redux-101.js
@@ -0,0 +1,48 @@
+import { createStore } from "redux";
+
+const incrementCount = ({ incrementBy = 1 } = {}) => {
+    return {
+        type: "INCREMENT",
+        incrementBy
+    }
+}
+
+const decrementCount = ({ decrementBy = 1 } = {}) => {
+    return {
+        type: "DECREMENT",
+        decrementBy
+    }
+}
+
+
+const store = createStore((state = {count : 0}, action) => {
+    switch (action.type) {
+        case "INCREMENT":
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+            return { 
+                count: state.count + incrementBy
+            };
+
+        case "DECREMENT":
+            return { 
+                count: state.count - 1 
+            };
+
+        case "RESET":
+            return { 
+                count: 0
+            };
+
+        default:
+            return state
+    }
+})
+
+console.log(store.getState());
+
+store.dispatch(incrementCount({ incrementBy: 100 }))
+store.dispatch(incrementCount())
+
+store.dispatch(decrementCount())
+// store.dispatch({type: "RESET"})
+console.log(store.getState());
